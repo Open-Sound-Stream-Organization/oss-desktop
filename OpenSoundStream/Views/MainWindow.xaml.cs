676bd818a4e4929d9603fad2ecd0ca1a80251202
Lisa Menacher <inf18155@lehre.dhbw-stuttgart.de>
2020-03-31 21:36:46 +0200

    #5 Create basic interface
    - Basic GUI without logic in WPF
    - Contains all needed elements for implementation of logic

--- OpenSoundStream/Views/MainWindow.xaml.cs
+++ OpenSoundStream/Views/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media.Imaging;
 using OpenSoundStream.ViewModel;
 
 namespace OpenSoundStream.Views
@@ -13,10 +14,36 @@ namespace OpenSoundStream.Views
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private static bool playing = false;
+
 		public MainWindow(MainViewModel t_mainWindowModel)
 		{
 			DataContext = t_mainWindowModel;
 			InitializeComponent();
 		}
+
+		private void Section_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+		}
+
+		private void StartPlayer(object sender, RoutedEventArgs e)
+		{
+			if (playing)
+			{
+				PlayButtonImage.Source = new BitmapImage(new Uri("./Icons/round_play_circle_filled_white_18dp.png", UriKind.Relative));
+				playing = false;
+			}
+			else
+			{
+				PlayButtonImage.Source = new BitmapImage(new Uri("./Icons/round_pause_white_18dp.png", UriKind.Relative));
+				playing = true;
+			}
+		}
+
+		private void adjustVolume(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			// MediaElement.Volume Propertie [0.00-1.00]
+		}
+
 	}
 }
